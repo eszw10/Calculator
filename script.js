246ee2cece0897c29895c885becbf86e3a08af10
eryc_s.z.w <erycszw10@gmail.com>
2023-01-01 14:07:28 +0700

    fix calculator style

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let btn = document.querySelectorAll('[cal-num]')
 let op = document.getElementById('operation')
 let result = document.getElementById('result')
+let firstNum = '';
+let lastNum = '';
 btn.forEach(button => {
     button.addEventListener('click',()=> {
         op.textContent += button.value
